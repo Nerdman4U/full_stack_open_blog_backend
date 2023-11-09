1375dd27248fed59129ebe00f9b5e9205c49d10c
Joni Toyryla <info@jonitoyryla.eu>
2023-11-09 17:18:28 +0200

    Osa 4: Tehtävä 4.4

--- tests/list_helpers.test.js
+++ tests/list_helpers.test.js
@@ -2,8 +2,32 @@ const listHelper = require('../utils/list_helpers')
 
 test('dummy returns one', () => {
   const blogs = []
-
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
 
+describe('total likes', () => {
+  test('It returns total likes', () => {
+    const blogs = [
+      { 'title': 'testi1', 'likes':1 },
+      { 'title': 'testi2', 'likes':2 },
+      { 'title': 'testi3', 'likes':3 }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(6)
+  })
+  test('It returns totals when only one', () => {
+    const blogs = [
+      { 'title': 'testi1', 'likes':12 }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(12)
+  })
+  test('It returns totals when empty', () => {
+    const blogs = [
+    ]
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(0)
+  })
+
+})
--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -1,8 +1,15 @@
-const dummy = (blogs) => {
-  return 1
+const logger = require('../utils/logger')
 
+const dummy = () => {
+  return 1
+}
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, item) => {
+    return sum + item.likes
+  },0)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
