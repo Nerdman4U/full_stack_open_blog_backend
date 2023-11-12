f6e96cd0184992ec4beab5c833a0d9be92bbc3f5
Joni Toyryla <info@jonitoyryla.eu>
2023-11-12 19:35:25 +0200

    Osa 4: User tests and controller.

--- tests/test_helpers.js
+++ tests/test_helpers.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialItems = [
   { 'title':'title1', 'author':'author1', 'url':'url1', 'likes':1 },
@@ -18,8 +19,15 @@ const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map((blog) => blog.toJSON() )
 }
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map((blog) => blog.toJSON() )
+}
 
 module.exports = {
-  initialItems, nonExistingId, blogsInDb
+  initialItems,
+  nonExistingId,
+  blogsInDb,
+  usersInDb
 }
 
